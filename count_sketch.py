759f8237dea8ce76974fb516aec56cd758ac65c3
Vista <retail3210@gmail.com>
2016-07-18 10:54:04 +0900

    Change build path

--- count_sketch.py
+++ count_sketch.py
@@ -1,10 +1,10 @@
 import tensorflow as tf
 
-_count_sketch_module = tf.load_op_library('./count_sketch.so')
+_count_sketch_module = tf.load_op_library('./build/count_sketch.so')
 _store = {}
 
 def count_sketch(probs, project_size):
-    """ Calculates count-min sketch of a probability distribution.
+    """ Calculates count-min sketch of a tensor.
     Args:
       probs: A `Tensor`
       project_size: output size (`int`)
@@ -16,11 +16,8 @@ def count_sketch(probs, project_size):
         input_size = int(probs.get_shape()[1])
 
         # h, s must be sampled once
-        if scope.name in _store:
-            print('reuse')
-            scope.reuse_variables()
+        if scope.name in _store: scope.reuse_variables()
         _store[scope.name] = True
-        print(_store)
 
         h = tf.get_variable('h', [input_size], initializer=tf.random_uniform_initializer(0, project_size), trainable=False)
         s = tf.get_variable('s', [input_size], initializer=tf.random_uniform_initializer(0, 2), trainable=False)
@@ -39,8 +36,8 @@ def _count_sketch_grad(op, grad):
     return [_count_sketch_module.count_sketch_grad(grad, h, s, input_size), None, None, None]
 
 def bilinear_pool(x1, x2, output_size):
-    """ Computes approximation of outer product with respect to x1, x2.
-    For details, see the original paper (https://arxiv.org/abs/1511.06062)
+    """ Computes approximation of bilinear pooling with respect to x1, x2.
+    For detailed explaination, see the paper (https://arxiv.org/abs/1511.06062)
 
     Args:
       x1: A `Tensor` with shape (batch_size, x1_size).
