e76a9652fd42e8a69c8b4a54dc6005776b07e730
Vista <retail3210@gmail.com>
2016-07-18 10:54:22 +0900

    Add unit test

--- test.py
+++ test.py
@@ -1,28 +1,36 @@
-from count_sketch import count_sketch, bilinear_pool
 import tensorflow as tf
+from count_sketch import count_sketch, bilinear_pool
 
-# vanila test.
-with tf.Session() as sess:
-    p1 = tf.constant([[0.83, 0.50, 0.10, 0.82, 0.24, 0.11, 0.21, 0.51, 0.39, 0.10, 0.67, 0.18]])
-    p2 = tf.constant([[0.18, 0.59, 0.56, 0.12, 0.33, 0.93, 0.17, 0.58, 0.16]])
-    d = 6
+class CountSketchTest(tf.test.TestCase):
+    def testGrad(self):
+        with self.test_session():
+            p = tf.constant([[0.83, 0.50, 0.10, 0.82, 0.24, 0.11, 0.21, 0.51, 0.39, 0.10, 0.67, 0.18]])
+            d = 6
+            sk = count_sketch(p, d)
+            tf.initialize_all_variables().run()
 
-    bcp = bilinear_pool(p1, p2, d)
-    sp1 = count_sketch(p1, d)
+            thg, numg = tf.test.compute_gradient(p, [1, 12], sk, [1, 6])
+            print('Theorical Gradient : {}'.format(thg))
+            print('Numerical Gradient : {}'.format(numg))
 
-    sess.run(tf.initialize_all_variables())
-    pp1, pbcp = sess.run([sp1, bcp])
+            err = tf.test.compute_gradient_error(p, [1, 12], sk, [1, 6])
+            print('SK Gradient Error : %.2f' % err)
+            self.assertTrue(err < 1e-3, 'Gradient must be matched with numerical gradient.')
 
-    # print('pp1=================')
-    # print(pp1)
-    # print('bcp================')
-    # print(pbcp)
 
-    der, num = tf.test.compute_gradient(p1, [1, 12], sp1, [1, 6])
-    print('Theorical Gradient : ' + str(der))
-    print('Numerical Gradient : ' + str(num))
+class BilinearPoolingTest(tf.test.TestCase):
+    def testGrad(self):
+        with self.test_session():
+            p1 = tf.constant([[0.83, 0.50, 0.10, 0.82, 0.24, 0.11, 0.21, 0.51, 0.39, 0.10, 0.67, 0.18]])
+            p2 = tf.constant([[0.18, 0.59, 0.56, 0.12, 0.33, 0.93, 0.17, 0.58, 0.16]])
+            d = 6
+            bp = bilinear_pool(p1, p2, d)
 
-    err = tf.test.compute_gradient_error(p1, [1, 12], sp1, [1, 6])
-    print('Gradient Error : %.2f' % err)
+            tf.initialize_all_variables().run()
 
-    assert err < 1e-2
+            err = tf.test.compute_gradient_error([p1, p2], ([1, 12], [1, 9]), bp, [1, 6])
+            print('BP Gradient Error : %.2f' % err)
+            self.assertTrue(err < 1e-3, 'Gradient must be matched with numerical gradient.')
+
+if __name__ == '__main__':
+    tf.test.main()
